de2a5357c05b0430394169bc52ffae21f1e6bf0d
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-06-10 15:13:33 +0000

    Deploying to gh-pages from @ Iryna-kondreskul/goit-js-hw-11@f9dbeb76c1dcde1ba35989766288c6e9447ef639 🚀

---
+++ index.71073cd9.js
@@ -0,0 +1,2 @@
+document.querySelector("search-form"),document.querySelector("submit"),document.querySelector("gallery");
+//# sourceMappingURL=index.71073cd9.js.map
---
+++ index.72da8550.js
@@ -0,0 +1,2 @@
+document.querySelector("search-form"),document.querySelector("submit"),document.querySelector("gallery");
+//# sourceMappingURL=index.72da8550.js.map
